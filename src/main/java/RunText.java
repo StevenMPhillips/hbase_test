4e01beac104ccf7caaa97b076677da2b27934020
Steven Phillips <sphillips@maprtech.com>
2014-11-14 00:56:16 -0800

    allow multiple threads for text

--- src/main/java/RunText.java
+++ src/main/java/RunText.java
@@ -32,14 +32,19 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextInputFormat;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class RunText {
+public class RunText implements Runnable {
 
   static Path path;
   static byte delim;
   static String toFind;
   static int index;
+  static int numThreads;
   static Options o;
+
+  private static AtomicInteger totalCount = new AtomicInteger();
+  private static AtomicInteger runningThreads;
 
   public static void main(String[] args) throws Exception {
     o = new Options();
@@ -61,27 +66,79 @@ public class RunText {
     delim = o.delimiter.getBytes()[0];
     toFind = o.value;
     index = o.index;
+    numThreads = o.threads;
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(conf);
     TextInputFormat format = new TextInputFormat();
-    FileSplit split = new FileSplit(path, 0L, fs.getFileStatus(path).getLen(), new String[] {""});
-    JobConf job = new JobConf();
-    job.setInputFormat(format.getClass());
-    RecordReader<LongWritable,Text> reader = format.getRecordReader(split, job, Reporter.NULL);
-    Text value = reader.createValue();
-    LongWritable key = reader.createKey();
-    int count = 0;
-    long t1 = System.nanoTime();
-    while(reader.next(key, value)) {
-      List<String> values = parse(value);
-      if (values.get(index).equals(toFind)) {
-        System.out.println(value);
-      }
-      count++;
+    long len = fs.getFileStatus(path).getLen() / numThreads;
+
+    List<Thread> threads = Lists.newArrayList();
+
+    for (int i = 0; i < numThreads; i++) {
+      FileSplit split = new FileSplit(path, i * len, len, new String[]{""});
+      threads.add(new Thread(new RunText(split, format)));
     }
-    long t2 = System.nanoTime();
-    System.out.printf("Read %d records in %f seconds. %f records per second", count, (t2 - t1) / 1E9, count * 1E9 / (t2 - t1));
+
+    runningThreads = new AtomicInteger(numThreads);
+
+    for (Thread t : threads) {
+      t.start();
+    }
+
+    int prev = 0;
+    int current;
+    long t1 = System.nanoTime();
+    long t2;
+    while (runningThreads.get() > 0) {
+      Thread.sleep(5000);
+      current = totalCount.get();
+      t2 = System.nanoTime();
+      System.out.println(String.format("%f records/sec", (current - prev) * 1e9 / (t2 - t1)));
+      t1 = t2;
+      prev = current;
+    }
+
+    for (Thread t: threads) {
+      t.join();
+    }
+
     fs.close();
+  }
+
+  private FileSplit split;
+  private TextInputFormat format;
+
+  public RunText(FileSplit split, TextInputFormat format) {
+    this.split = split;
+    this.format = format;
+  }
+
+  @Override
+  public void run() {
+    try {
+      JobConf job = new JobConf();
+      job.setInputFormat(format.getClass());
+      RecordReader<LongWritable, Text> reader = format.getRecordReader(split, job, Reporter.NULL);
+      Text value = reader.createValue();
+      LongWritable key = reader.createKey();
+      int count = 0;
+      long t1 = System.nanoTime();
+      while (reader.next(key, value)) {
+        List<String> values = parse(value);
+        if (values.get(index).equals(toFind)) {
+          System.out.println(value);
+        }
+        count++;
+        if (count == 100) {
+          totalCount.addAndGet(100);
+          count = 0;
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      runningThreads.decrementAndGet();
+    }
   }
 
   private static List<String> parse(Text value) {
@@ -116,6 +173,7 @@ public class RunText {
     return -1;
   }
 
+
   static class Options {
     @Parameter(names = {"-p"}, description = "path", required=true)
     public String path = null;
@@ -128,6 +186,9 @@ public class RunText {
 
     @Parameter(names = {"-i"}, description = "index", required=true)
     public int index = 0;
+
+    @Parameter(names = {"-t"}, description = "threads", required=true)
+    public int threads = 0;
 
     @Parameter(names = {"-h", "--help"}, description = "show usage", help=true)
     public boolean help = false;
